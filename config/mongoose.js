d3bd907293ce5bf6f2af53095bae18a39aa6435e
Pankaj <pankajbeniwal3112@gmail.com>
2023-08-11 13:17:09 +0530

    add mongo atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function main(){
     try{
-        const db = await mongoose.connect('mongodb://127.0.0.1:27017/csv-upload');
+        const db = await mongoose.connect('mongodb+srv://pankaj3112:ftHLwuuPqJvyE9Qj@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
         module.exports = db;
         console.log('**** MongoDB Connected ****')
     }
